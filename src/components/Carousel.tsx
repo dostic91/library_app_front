53cd60d4ebaaa0550849bd1e73b361e40f1cbf63
dostic91 <dosticdejan@gmail.com>
2023-01-22 22:33:30 +0100

    test1

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -11,7 +11,7 @@ export const Carousel = () => {
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const baseUrl: string = "http://localhost:6060/api/books";
+      const baseUrl: string = "http://localhost:7070/api/books";
 
       const url: string = `${baseUrl}?page=0&size=9`;
 
