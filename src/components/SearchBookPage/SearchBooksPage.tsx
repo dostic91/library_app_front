5d24fbfed8fa783a7986790da65b800be6057d6e
dostic91 <dosticdejan@gmail.com>
2023-01-23 00:03:17 +0100

    category selection

--- src/components/SearchBookPage/SearchBooksPage.tsx
+++ src/components/SearchBookPage/SearchBooksPage.tsx
@@ -10,15 +10,24 @@ export const SearchBooksPage = () => {
     const[isLoading, setIsLoading] = useState(true);
     const[httpError, setHttpError] = useState(null);
     const[currentPage, setCurrentPage] = useState(1);
-    const[booksPerPage, setBooksPerPage] = useState(5);
+    const[booksPerPage] = useState(5);
     const[totalAmountOfBooks, setTotalAmountsOfBooks] = useState(0);
     const[totalPages, setTotalPages] = useState(0);
+    const[search, setSearch] = useState('');
+    const[searchUrl, setSearchUrl] = useState('');
+    const[categorySelection, setCategorySelection] = useState('');
 
     useEffect(() => {
         const fetchBooks = async () => {
           const baseUrl: string = "http://localhost:7070/api/books";
     
-          const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+          let url: string = ``;
+
+          if(searchUrl === '') {
+            url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+          } else {
+            url = baseUrl + searchUrl;
+          }
     
           const response = await fetch(url);
     
@@ -57,7 +66,7 @@ export const SearchBooksPage = () => {
           setHttpError(error.message);
         })
         window.scrollTo(0, 0);
-      }, [currentPage]);
+      }, [currentPage, searchUrl]);
 
       if(isLoading) {
         return(
@@ -74,6 +83,28 @@ export const SearchBooksPage = () => {
         )
       }
 
+      const searchHandleChange = () => {
+        if(search === '') {
+            setSearchUrl('');
+        } else {
+          setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`);
+        }
+      }
+
+      const categoryField = (value: any) => {
+        if(value.toLowerCase() === 'fe' || 
+           value.toLowerCase() === 'be' ||
+           value.toLowerCase() === 'data' ||
+           value.toLowerCase() === 'devops') {
+            setCategorySelection(value);
+            setSearchUrl(`/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`)
+           } else {
+            setCategorySelection('All');
+            setSearchUrl(`?page=0&size=${booksPerPage}`)
+           }
+
+      }
+
       const indexOfLastBook: number = currentPage * booksPerPage;
       const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
       let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ? booksPerPage * currentPage : totalAmountOfBooks;
@@ -86,35 +117,35 @@ export const SearchBooksPage = () => {
                 <div className="row mt-5">
                     <div className="col-6">
                         <div className="d-flex">
-                            <input className='form-control me-2' type="search" placeholder='Search' aria-labelledby='Search'></input>
-                            <button className='btn btn-outline-success'></button>
+                            <input className='form-control me-2' type="search" placeholder='Search' aria-labelledby='Search' onChange={e => setSearch(e.target.value)}></input>
+                            <button className='btn btn-outline-success' onClick={() => searchHandleChange()}>Search</button>
                         </div>
                     </div>
                     <div className="col-4">
                         <div className="dropdown">
                             <button className='btn btn-secondary dropdown-toggle' type='button' id='dropwdownMenuButton1' data-bs-toggle="dropdown" aria-expanded="false">Category</button>
                             <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
-                                <li>
+                                <li onClick={() => categoryField("All")}>
                                     <a className='dropdown-item' href='#'>
                                         All
                                     </a>
                                 </li>
-                                <li>
+                                <li onClick={() => categoryField("fe")}>
                                     <a className='dropdown-item' href='#'>
                                         Front End
                                     </a>
                                 </li>
-                                <li>
+                                <li onClick={() => categoryField("be")}>
                                     <a className='dropdown-item' href='#'>
                                         Back End
                                     </a>
                                 </li>
-                                <li>
+                                <li onClick={() => categoryField("data")}>
                                     <a className='dropdown-item' href='#'>
                                         Data
                                     </a>
                                 </li>
-                                <li>
+                                <li onClick={() => categoryField("devops")}>
                                     <a className='dropdown-item' href='#'>
                                         DevOps
                                     </a>
@@ -123,13 +154,24 @@ export const SearchBooksPage = () => {
                         </div>
                     </div>
                 </div>
+                {totalAmountOfBooks > 0 ? 
+                <>
                 <div className="mt-3">
                     <h5>Number of results: ({totalAmountOfBooks})</h5>
                 </div>
-                <p>{indexOfFirstBook + 1} to{" " + lastItem} of {totalAmountOfBooks} of 22 items:</p>
+                <p>{indexOfFirstBook + 1} to{" " + lastItem} of {totalAmountOfBooks}</p>
                 {books.map(book => (
                     <SearchBook book={book} key={book.id} />
                 ))}
+                </>
+                :
+                <div className='m-5'>
+                    <h3>
+                        Can't find what are you looking for?
+                    </h3>
+                    <a type='button' className='btn main-color btn-md px-4 me-md-2 fw-bold text-white' href='#'>Library Services</a>
+                </div>
+                } 
                 {totalPages > 1 && 
                     <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate}/>
                 }
